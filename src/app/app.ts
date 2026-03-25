a6d5c15bf8600e08a65e7eec2ead82609032889a
Arkadiusz-Zwolenik <arkadiusz.zwolenik@gotoma.pl>
2026-03-25 09:27:17 +0100

    Loading model from File

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,15 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, ElementRef, ViewChild, AfterViewInit, HostListener, PLATFORM_ID, inject } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  HostListener,
+  PLATFORM_ID,
+  inject,
+} from '@angular/core';
 import * as THREE from 'three';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 @Component({
   selector: 'app-root',
@@ -31,6 +40,46 @@ export class App implements AfterViewInit {
     this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(width, height);
+  }
+
+  public onFileSelected(event: Event): void {
+    const input: HTMLInputElement = event.target as HTMLInputElement;
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+    const file: File = input.files[0];
+    const url: string = URL.createObjectURL(file);
+    const loader = new GLTFLoader();
+    loader.load(
+      url,
+      (gltf: GLTF) => {
+        const model = gltf.scene;
+        this.clearScene();
+        this.scene.add(model);
+        const box = new THREE.Box3().setFromObject(model);
+        const size = box.getSize(new THREE.Vector3());
+        const center = box.getCenter(new THREE.Vector3());
+        model.position.sub(center);
+        const maxDim = Math.max(size.x, size.y, size.z);
+        const fov = this.camera.fov * (Math.PI / 180);
+        let cameraZ = Math.abs(maxDim / 2 / Math.tan(fov / 2));
+        cameraZ *= 1.5;
+        this.camera.position.set(0, 0, cameraZ);
+        this.camera.lookAt(0, 0, 0);
+        this.camera.updateProjectionMatrix();
+        console.log('Model loaded from file');
+      },
+      undefined,
+      (error: unknown) => {
+        console.error('Error loading file:', error);
+      },
+    );
+  }
+
+  private clearScene() {
+    this.scene.children = this.scene.children.filter((obj) => {
+      return obj.type.includes('Light');
+    });
   }
 
   private initThree(): void {
