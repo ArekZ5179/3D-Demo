734c8673876068896add9895d249dae773f56802
Arkadiusz-Zwolenik <arkadiusz.zwolenik@gotoma.pl>
2026-03-25 09:05:42 +0100

    Scene with cube

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,60 @@
-import { Component, ElementRef, signal, ViewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, ViewChild, AfterViewInit, HostListener, PLATFORM_ID, inject } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements AfterViewInit {
+  private platformId = inject(PLATFORM_ID);
   @ViewChild('sceneContainer', { static: true })
-  sceneContainer!: ElementRef<HTMLDivElement>;
+  containerRef!: ElementRef<HTMLDivElement>;
+  private scene!: THREE.Scene;
+  private camera!: THREE.PerspectiveCamera;
+  private renderer!: THREE.WebGLRenderer;
+  private cube!: THREE.Mesh;
 
   ngAfterViewInit(): void {
-    const container = this.sceneContainer.nativeElement;
-    console.log('Scene container ready:', container);
+    if (isPlatformBrowser(this.platformId)) {
+      this.initThree();
+      this.animate();
+    }
   }
+
+  @HostListener('window:resize')
+  onResize() {
+    const container = this.containerRef.nativeElement;
+    const width = container.clientWidth;
+    const height = container.clientHeight;
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(width, height);
+  }
+
+  private initThree(): void {
+    const container: HTMLDivElement = this.containerRef.nativeElement;
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x0f172a);
+    const width: number = container.clientWidth;
+    const height: number = container.clientHeight;
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.camera.position.z = 5;
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(width, height);
+    container.appendChild(this.renderer.domElement);
+    const geometry: THREE.BoxGeometry = new THREE.BoxGeometry();
+    const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x3b82f6 });
+    const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(cube);
+    this.cube = cube;
+  }
+
+  private animate = () => {
+    requestAnimationFrame(this.animate);
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+    this.renderer.render(this.scene, this.camera);
+  };
 }
